74d3ac3bbffdae7f791d1600c72cc9932d310d8a
Łukasz Rzepka <lukas.rzepka@uj.edu.pl>
2016-05-25 20:43:18 +0200

    Make polynomials simpler

--- src/main/java/pl/jagiellonian/Models/PolynomialAsMap.java
+++ src/main/java/pl/jagiellonian/Models/PolynomialAsMap.java
@@ -9,58 +9,42 @@ import java.util.Map;
  * Created by lukaszrzepka on 11.05.2016.
  */
 public class PolynomialAsMap {
-    private Map<Map<List<Integer>, Integer>, Integer> polynomialMap;
+    private Map<List<Integer>, Integer> polynomialMap;
 
-    public PolynomialAsMap(Map<Map<List<Integer>, Integer>, Integer> polynomialMap) {
+    public PolynomialAsMap(Map<List<Integer>, Integer> polynomialMap) {
         this.polynomialMap = polynomialMap;
     }
 
-    public Map<Map<List<Integer>, Integer>, Integer> getPolynomialMap() {
+    public Map<List<Integer>, Integer> getPolynomialMap() {
         return polynomialMap;
     }
 
-    public void setPolynomialMap(Map<Map<List<Integer>, Integer>, Integer> polynomialMap) {
+    public void setPolynomialMap(Map<List<Integer>, Integer> polynomialMap) {
         this.polynomialMap = polynomialMap;
     }
 
-    public PolynomialAsMap multipleWith(Map<Map<List<Integer>, Integer>, Integer> polynomialMap) {
-        Map<Map<List<Integer>, Integer>, Integer> polynomialsResult = new HashMap<>();
-        Map<List<Integer>, Integer> monomialResult = new HashMap<>();
+    public PolynomialAsMap multipleWith(Map<List<Integer>, Integer> polynomialMap) {
+        Map<List<Integer>, Integer> polynomialResult = new HashMap<>();
 
-        for (Map.Entry<Map<List<Integer>, Integer>, Integer> polynomials : polynomialMap.entrySet()) {
-            Map<List<Integer>, Integer> polynomial = polynomials.getKey();
-            Integer polynomialCoefficient = polynomials.getValue();
+        for (Map.Entry<List<Integer>, Integer> polynomial : polynomialMap.entrySet()) {
+            List<Integer> monomialPowers = polynomial.getKey();
+            Integer monomialCoefficient = polynomial.getValue();
 
-            for (Map.Entry<List<Integer>, Integer> monomial : polynomial.entrySet()) {
-                List<Integer> monomialPowers = monomial.getKey();
-                Integer monomialCoefficient = monomial.getValue();
+            for (Map.Entry<List<Integer>, Integer> thisPolynomial : this.polynomialMap.entrySet()) {
+                List<Integer> thisMonomialPowers = thisPolynomial.getKey();
+                Integer thisMonomialCoefficient = thisPolynomial.getValue();
 
-                for (Map.Entry<Map<List<Integer>, Integer>, Integer> thisPolynomialMap : this.polynomialMap.entrySet()) {
-                    Map<List<Integer>, Integer> thisPolynomial = thisPolynomialMap.getKey();
-                    Integer thisPolynomialCoefficient = thisPolynomialMap.getValue();
+                List<Integer> monomialPowersResult = new ArrayList<>();
 
-                    for (Map.Entry<List<Integer>, Integer> thisMonomial : thisPolynomial.entrySet()) {
-                        List<Integer> thisMonomialPowers = thisMonomial.getKey();
-                        Integer thisMonomialCoefficient = thisMonomial.getValue();
-
-                        List<Integer> monomialPowersResult = new ArrayList<>();
-
-                        for (int i = 0; i < thisMonomialPowers.size(); i++) {
-                            monomialPowersResult.add(thisMonomialPowers.get(i) + monomialPowers.get(i));
-                        }
-
-                        Integer monomialResultCoefficient = thisMonomialCoefficient * monomialCoefficient;
-                        monomialResult.put(monomialPowersResult, monomialResultCoefficient);
-
-                    }
+                for (int i = 0; i < thisMonomialPowers.size(); i++) {
+                    monomialPowersResult.add(thisMonomialPowers.get(i) + monomialPowers.get(i));
                 }
+
+                Integer monomialResultCoefficient = thisMonomialCoefficient * monomialCoefficient;
+                polynomialResult.put(monomialPowersResult, monomialResultCoefficient);
             }
         }
-        Integer polynomialResultCoefficient = 1;
-        polynomialsResult.put(monomialResult, polynomialResultCoefficient);
-
-        PolynomialAsMap polynomialAsMap = new PolynomialAsMap(polynomialsResult);
-
+        PolynomialAsMap polynomialAsMap = new PolynomialAsMap(polynomialResult);
         return polynomialAsMap;
     }
 
--- src/test/java/pl/jagiellonian/PolynomialAsMapTest.java
+++ src/test/java/pl/jagiellonian/PolynomialAsMapTest.java
@@ -16,30 +16,24 @@ public class PolynomialAsMapTest {
         Integer polynomialCoefficient2 = 1;
 
 
-        //(-(b c^2) + 4 (a c^3) - 3 c^2)
-        Map<List<Integer>, Integer> monomials = new HashMap<>();
-        monomials.put(new ArrayList<>(Arrays.asList(0,1,2)), -1);
-        monomials.put(new ArrayList<>(Arrays.asList(1,0,3)), 4);
-        monomials.put(new ArrayList<>(Arrays.asList(0,0,2)), -3);
+        // -(b c^2) + 4 (a c^3) - 3 c^2
+        Map<List<Integer>, Integer> polynomial1 = new HashMap<>();
+        polynomial1.put(new ArrayList<>(Arrays.asList(0,1,2)), -1);
+        polynomial1.put(new ArrayList<>(Arrays.asList(1,0,3)), 4);
+        polynomial1.put(new ArrayList<>(Arrays.asList(0,0,2)), -3);
 
-        Map<Map<List<Integer>, Integer>, Integer> polynomials = new HashMap<>();
-        polynomials.put(monomials, polynomialCoefficient1);
-
-        PolynomialAsMap polynomialMap1 = new PolynomialAsMap(polynomials);
+        PolynomialAsMap polynomialMap1 = new PolynomialAsMap(polynomial1);
 
 
-        //(3 (b c) - a^2 b^2)
-        Map<List<Integer>, Integer> monomials2 = new HashMap<>();
-        monomials2.put(new ArrayList<>(Arrays.asList(0,1,1)), 3);
-        monomials2.put(new ArrayList<>(Arrays.asList(2,2,0)), -1);
-
-        Map<Map<List<Integer>, Integer>, Integer> polynomials2 = new HashMap<>();
-        polynomials2.put(monomials2, polynomialCoefficient2);
+        // 3 (b c) - a^2 b^2
+        Map<List<Integer>, Integer> polynomial2 = new HashMap<>();
+        polynomial2.put(new ArrayList<>(Arrays.asList(0,1,1)), 3);
+        polynomial2.put(new ArrayList<>(Arrays.asList(2,2,0)), -1);
 
 
-        //(-(b c^2) + 4 (a c^3) - 3 c^2) * (3 (b c) - a^2 b^2)
-        PolynomialAsMap polynomialAsMap2 = polynomialMap1.multipleWith(polynomials2);
-        assertEquals("{{[0, 2, 3]=-3, [2, 3, 2]=1, [0, 1, 3]=-9, [2, 2, 2]=3, [1, 1, 4]=12, [3, 2, 3]=-4}=1}", polynomialAsMap2.toString());
+        // (-(b c^2) + 4 (a c^3) - 3 c^2) * (3 (b c) - a^2 b^2)
+        PolynomialAsMap polynomialAsMap2 = polynomialMap1.multipleWith(polynomial2);
+        assertEquals("{[0, 2, 3]=-3, [2, 3, 2]=1, [0, 1, 3]=-9, [2, 2, 2]=3, [1, 1, 4]=12, [3, 2, 3]=-4}", polynomialAsMap2.toString());
         System.out.println(polynomialAsMap2);
         //-4 a^3 b^2 c^3   +   a^2 b^3 c^2   +   3 a^2 b^2 c^2   +   12 a b c^4   -   3 b^2 c^3   -   9 b c^3
     }
