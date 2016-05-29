1b46d075bbf1240e400ec84c65b77c3144940a0d
Anna <annna.gawlowska@gmail.com>
2016-05-29 19:47:56 +0200

    Unit test and empty classes for monomial order implementation

---
+++ src/main/java/pl/jagiellonian/implementation/LexicographicOrderComparator.java
@@ -0,0 +1,12 @@
+package pl.jagiellonian.implementation;
+
+import pl.jagiellonian.interfaces.IVariable;
+import pl.jagiellonian.interfaces.MonomialComparator;
+
+public class LexicographicOrderComparator implements MonomialComparator {
+    @Override
+    public boolean compare(IVariable expr1, IVariable expr2) {
+        //TODO
+        return false;
+    }
+}
---
+++ src/main/java/pl/jagiellonian/interfaces/MonomialComparator.java
@@ -0,0 +1,5 @@
+package pl.jagiellonian.interfaces;
+
+public interface MonomialComparator {
+    boolean compare(IVariable expr1, IVariable expr2);
+}
