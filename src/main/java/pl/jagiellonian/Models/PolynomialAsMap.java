3ad53b048dce41b97dc85d64eae6ab11985552b1
Labaj <damianlabaj@op.pl>
2016-06-05 17:25:48 +0200

    Implementation of lexicographic order

--- src/main/java/pl/jagiellonian/Models/PolynomialAsMap.java
+++ src/main/java/pl/jagiellonian/Models/PolynomialAsMap.java
@@ -2,20 +2,19 @@ package pl.jagiellonian.Models;
 
 import pl.jagiellonian.utils.MonomialOrder;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by lukaszrzepka on 11.05.2016.
  */
 public class PolynomialAsMap {
     private Map<List<Integer>, Integer> polynomialMap;
-    private MonomialOrder monomialOrder = MonomialOrder.GRADED_LEXICOGRAPHIC;
+    private MonomialOrder monomialOrder;
 
     public PolynomialAsMap(Map<List<Integer>, Integer> polynomialMap) {
         this.polynomialMap = polynomialMap;
+        this.monomialOrder = MonomialOrder.LEXICOGRAPHIC;
     }
 
     public Map<List<Integer>, Integer> getPolynomialMap() {
@@ -62,13 +61,57 @@ public class PolynomialAsMap {
 
     @Override
     public String toString() {
+        final Comparator<List<Integer>> exponentsComparator = new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                int temp;
+                for (int i = 0; i < o1.size(); i++) {
+                    temp = Integer.compare(o1.get(i), o2.get(i));
+                    if (temp != 0) return -temp;
+                }
+                return 0;
+            }
+        };
+
+        String output = "";
         switch (monomialOrder) {
             case LEXICOGRAPHIC:
-                return "";
-
+                output = polynomialMap.entrySet().stream()
+                            .sorted((o1, o2) -> exponentsComparator.compare(o1.getKey(), o2.getKey()))
+                            .map(PolynomialAsMap::getMonomialFromEntry)
+                            .collect(Collectors.joining(" "));
+                break;
             case GRADED_LEXICOGRAPHIC:
-            default:
-                return "";
+
+                break;
         }
+        return output.startsWith("+ ") ? output.substring(2) : output;
+    }
+
+    private static String getMonomialFromEntry(Map.Entry<List<Integer>, Integer> entry) {
+        final StringBuilder builder = new StringBuilder();
+
+        boolean skipValue = Math.abs(entry.getValue()) == 1;
+        if (entry.getValue() > 0) {
+            builder.append("+ ");
+            if (!skipValue) builder.append(entry.getValue());
+        }
+        else if (entry.getValue() < 0) {
+            builder.append("- ");
+            if (!skipValue) builder.append(Math.abs(entry.getValue()));
+        }
+        else return "";
+
+        for (int i = 0; i < entry.getKey().size(); i++) {
+            if (entry.getKey().get(i) != 0) {
+                if (!skipValue) builder.append("*");
+                builder.append("x_" + (i+1));
+                if (entry.getKey().get(i) != 1) {
+                    builder.append("^" + entry.getKey().get(i));
+                }
+                skipValue = false;
+            }
+        }
+        return builder.toString();
     }
 }
--- src/test/java/pl/jagiellonian/PolynomialAsMapTest.java
+++ src/test/java/pl/jagiellonian/PolynomialAsMapTest.java
@@ -33,7 +33,7 @@ public class PolynomialAsMapTest {
 
         // (-(b c^2) + 4 (a c^3) - 3 c^2) * (3 (b c) - a^2 b^2)
         PolynomialAsMap polynomialAsMap2 = polynomialMap1.multipleWith(new PolynomialAsMap(polynomial2));
-        assertEquals("{[0, 2, 3]=-3, [2, 3, 2]=1, [0, 1, 3]=-9, [2, 2, 2]=3, [1, 1, 4]=12, [3, 2, 3]=-4}", polynomialAsMap2.toString());
+        assertEquals("{[0, 2, 3]=-3, [2, 3, 2]=1, [0, 1, 3]=-9, [2, 2, 2]=3, [1, 1, 4]=12, [3, 2, 3]=-4}", polynomialAsMap2.getPolynomialMap().toString());
         System.out.println(polynomialAsMap2);
         //-4 a^3 b^2 c^3   +   a^2 b^3 c^2   +   3 a^2 b^2 c^2   +   12 a b c^4   -   3 b^2 c^3   -   9 b c^3
     }
